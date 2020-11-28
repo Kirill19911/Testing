060cc36c211b6da8b6265b88cdf491c114ecb402
Kirill19911 <45298997+Kirill19911@users.noreply.github.com>
2020-11-28 23:10:26 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,30 @@
+import requests
+from tabulate import tabulate
+
+#assign date and currency base that you need
+
+date="2010-01-15"
+base="USD"
+
+r=requests.get(
+    f"https://api.exchangeratesapi.io/{date}", params={"base":base}
+)
+r.raise_for_status()
+print(r.status_code)
+
+dict_day=r.json()
+dict_rates=dict_day["rates"]
+
+columns=["Currency", "Exchange Rate (base "+base+")"]
+table_list=[]
+for key, value in dict_rates.items():
+    line=[str(key), str(value)]
+    table_list.append(line)
+
+
+with open("rates.html", "w") as f:
+    f.write("<html>")
+    f.write("<body>")
+    f.write(tabulate(table_list, headers=columns, tablefmt="html", stralign="center"))
+    f.write("</body>")
+    f.write("</html>")
